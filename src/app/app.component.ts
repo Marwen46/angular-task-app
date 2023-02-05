983ac7208966e5496780abf25cc20eae1d4df0d8
Marouene <marwen.ayoub@outlook.com>
2023-02-05 12:21:39 +0100

    pushing to git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tasks-app';
+  constructor(private usersService: UsersService) {}
+
+  activeUsers: string[] = this.usersService.activeUsers;
+  inactiveUsers: string[] = this.usersService.inactiveUsers;
+
+  onSetToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+
+  onSetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
 }
